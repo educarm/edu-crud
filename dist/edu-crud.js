639274c5e697805faae9775c9071ab4bdf44bf58
delafuente28 <delafuente.brando@gmail.com>
2015-01-07 14:48:59 +0100

    Cambios en updateFields()

--- dist/edu-crud.js
+++ dist/edu-crud.js
@@ -149,6 +149,10 @@ eduCrudDirectives.directive('eduCrud', function () {
         $scope.internalControl.refresh = function () {
           $scope.options.gridControl.refresh();
         };
+        $scope.internalControl.updateFields = function () {
+          $scope.options.gridControl.updateFields();
+          $scope.updateFields();
+        };
         $scope.internalControl.showOverlayLoading = function (bShow) {
           $scope.options.gridControl.showOverlayLoading(bShow);
         };
@@ -213,6 +217,13 @@ eduCrudDirectives.directive('eduCrud', function () {
         $scope.options.listListeners.onExtraButtonClick = function () {
           console.log('click extra button:');
           $scope.add();
+        };
+        //Inicializa la lista de campos para que funcionen correctamente.
+        $scope.updateFields = function () {
+          if (typeof $scope.options.crudUri !== 'undefined' && $scope.options.crudUri !== '') {
+            $scope.api = dataFactoryCrud($scope.options.crudUri, typeof $scope.options.actions !== 'undefined' ? $scope.options.actions : '');
+          }
+          ;
         };
         if ($scope.options.showButtonsUserPre) {
           if ($scope.options.hasOwnProperty('buttonsUserPre')) {
